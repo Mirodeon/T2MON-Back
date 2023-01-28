c4368da66691fedf397df83f89d62ff613dc9720
Mirodeon <mehdi.habaru@gmail.com>
2023-01-28 20:57:00 +0100

    cleaning health-check

--- core/healthCheck/viewsets.py
+++ core/healthCheck/viewsets.py
@@ -1,8 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
 from rest_framework import status
-from rest_framework import viewsets
 from rest_framework.decorators import api_view
 
 
