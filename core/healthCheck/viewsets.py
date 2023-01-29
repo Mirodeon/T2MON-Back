e8f571e50f945a99328444746410d66ecb70946b
Mirodeon <mehdi.habaru@gmail.com>
2023-01-29 01:09:15 +0100

    add check admin at health check

--- core/healthCheck/viewsets.py
+++ core/healthCheck/viewsets.py
@@ -1,8 +1,12 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from core.user.models import User
 
 
 @api_view(['GET'])
 def HealthCheck(self):
-    return Response(status=status.HTTP_200_OK)
+    if User.objects.filter(username='Admin').exists():
+        return Response(status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
